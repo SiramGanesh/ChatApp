a5b613cbdcc99efc78e0ce7baba6fb3922f387bf
Siram Ganesh <navadeepsaiganeshsiram@gmail.com>
2026-07-16 15:30:13 +0530

    feat: add axios instance and utility functions; implement authentication store with socket.io integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,28 @@ import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
 import PageLoader from './components/PageLoader';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
+
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if(!isLoaded) return;
+
+    if(isSignedIn) checkAuth();
+    else clearAuth();
+
+  },[checkAuth,clearAuth,isLoaded,isSignedIn])
+
   
-  if(!isLoaded) {
+  if(!isLoaded || (isSignedIn && isCheckingAuth)) {
     return <PageLoader />
   }
 
@@ -21,6 +37,7 @@ function App() {
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />} />
           <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   )
